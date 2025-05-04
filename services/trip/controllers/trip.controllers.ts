4af3b014b41efd64532736513149eff6d9e6f6b4
angel <angel.rodriguez3rm@gmail.com>
2025-05-04 19:48:36 +0200

    trip service actions

--- services/trip/controllers/trip.controllers.ts
+++ services/trip/controllers/trip.controllers.ts
@@ -1,5 +1,10 @@
 import { Request, Response } from "express";
-import { getUserTrips } from "../servicies/trip.service";
+import {
+  getUserTripsFromDB,
+  getTripByIdFromDB,
+  updateTripFromDB,
+  deleteTripFromDB,
+} from "../servicies/trip.service";
 
 export const getTrips = async (req: Request, res: Response): Promise<void> => {
   const userId = req.headers["x-user-id"];
@@ -11,8 +16,64 @@ export const getTrips = async (req: Request, res: Response): Promise<void> => {
   }
 
   try {
-    const trips = await getUserTrips(userId);
+    const trips = await getUserTripsFromDB(userId);
     res.status(200).json(trips);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const getTripById = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const userId = req.headers["x-user-id"];
+  const tripId = req.params.id;
+  if (!userId || typeof userId !== "string") {
+    res.status(400).json({ error: "x-user-id header missing" });
+    return;
+  }
+  try {
+    const trip = await getTripByIdFromDB(userId, tripId);
+    res.status(200).json(trip);
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const updateTrip = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const userId = req.headers["x-user-id"];
+  const tripId = req.params.id;
+  const updates = req.body;
+  if (!userId || typeof userId !== "string") {
+    res.status(400).json({ error: "x-user-id header missing" });
+    return;
+  }
+
+  try {
+    await updateTripFromDB(userId, tripId, updates);
+    res.status(200).json({ message: "Trip updated successfully" });
+  } catch (error: any) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+export const deleteTrip = async (
+  req: Request,
+  res: Response
+): Promise<void> => {
+  const userId = req.headers["x-user-id"];
+  const tripId = req.params.id;
+  if (!userId || typeof userId !== "string") {
+    res.status(400).json({ error: "x-user-id header missing" });
+    return;
+  }
+  try {
+    await deleteTripFromDB(userId, tripId);
+    res.status(200).json({ message: "Trip deleted successfully" });
   } catch (error: any) {
     res.status(500).json({ error: error.message });
   }
--- services/trip/routes/trip.routes.ts
+++ services/trip/routes/trip.routes.ts
@@ -1,8 +1,16 @@
 import { Router } from "express";
-import { getTrips } from "../controllers/trip.controllers";
+import {
+  getTrips,
+  getTripById,
+  updateTrip,
+  deleteTrip,
+} from "../controllers/trip.controllers";
 
 const router = Router();
 
 router.get("/", getTrips);
+router.get("/:id", getTripById);
+router.put("/:id", updateTrip);
+router.delete("/:id", deleteTrip);
 
 export default router;
