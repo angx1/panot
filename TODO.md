b5b1bb7ea470b0fc120bd3aaa49a595ff8e74660
angel <angel.rodriguez3rm@gmail.com>
2025-05-05 12:50:11 +0200

    schema separation for microservices

--- TODO.md
+++
@@ -1,1 +0,0 @@
-1. controller y routes en servicio trips
--- services/trip/supabaseClient.ts
+++ services/trip/supabaseClient.ts
@@ -6,4 +6,6 @@ dotenv.config();
 const supabaseUrl = process.env.SUPABASE_URL!;
 const supabaseKey = process.env.SUPABASE_SERVICE_ROLE_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey, {
+  db: { schema: "trip" },
+});
